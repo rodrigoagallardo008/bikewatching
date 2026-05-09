41716a017880769726147e625fadf06d578976f0
Rodrigo Gallardo <rodrigo.gallardo008@gmail.com>
2026-05-08 21:28:08 -0400

    Mapbox Set Up

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  import mapboxgl from "mapbox-gl";
+  import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+  import { onMount } from "svelte";
+
+  mapboxgl.accessToken = "pk.eyJ1Ijoicm9kcmlnb2FnYWxsYXJkbyIsImEiOiJjbW94bmZrenkwMWlhMnFxNGQ2M2QyaTJnIn0.GvNK5oaTMDx7p0LnrzVYPg";
+
+  let map;
+
+  async function initMap() {
+    map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-71.09415, 42.36027],
+      zoom: 12,
+    });
+    await new Promise(resolve => map.on("load", resolve));
+  }
+
+  onMount(() => {
+    initMap();
+  });
+</script>
+
+<h1>🚲 BikeWatch</h1>
+
+<div id="map"></div>
+
+<style>
+  @import url("$lib/global.css");
+
+  #map {
+    flex: 1;
+    min-height: 600px;
+  }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/bikewatching',
+    },
+  },
+};
+
+export default config;
